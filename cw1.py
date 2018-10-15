2c34bafbc34ba667e3ecdd77b8c0a5f5661fa6de
EnriqueBC <belenguercarrascoenrique@gmail.com>
2018-10-15 19:34:43 +0100

    Mondaynight

--- cw1.py
+++ cw1.py
@@ -1,4 +1,4 @@
-from flask import Flask , render_template, request
+from flask import Flask , render_template, request, url_for,redirect, flash
 app = Flask(__name__)
 
 @app.route('/')
@@ -14,6 +14,22 @@ def uploading():
         return "File Uploaded"
     else:
         return render_template('uploading.html')
+
+@app.route("/genre/drama/theshawshankredemtion/")
+@app.route("/movies/theshawshankredemtion/")
+def theshawshankredemtion():
+    movie='The Shawshank Redemption'
+    poster= url_for('static', filename='posters/0000000001.jpg')
+    director='Frank Darabont'
+    producer='Niki Marvin'
+    genre='Drama'
+    year='1994'
+    plot=''
+    time='48:49'
+    casting=['Tim Robbins','Morgan Freeman','Bob Gunton','William Sadler','Clancy Brown','Gil Bellows','James Whitmore']
+    budget='$25 millions'
+    boxoffice='$53.8 millions'
+    return render_template('tableshow.html',movie=movie,poster=poster,director=director,producer=producer,genre=genre,year=year,plot=plot,time=time,casting=casting,budget=budget,boxoffice=boxoffice)
 
 
 
