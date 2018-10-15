db2dbf3e079b7c718b48651b5483cebfbbdb7c0e
EnriqueBC <belenguercarrascoenrique@gmail.com>
2018-10-15 17:18:21 +0100

    Monday

--- cw1.py
+++ cw1.py
@@ -1,9 +1,21 @@
-from flask import Flask , render_template
+from flask import Flask , render_template, request
 app = Flask(__name__)
 
 @app.route('/')
 def root():
     return render_template('base.html'),200
+
+#uploading data
+@app.route("/uploading/", methods=['POST','GET'])
+def uploading():
+    if request.method == 'POST':
+        f = request.files['datafile']
+        f.save('static/img/file.png')
+        return "File Uploaded"
+    else:
+        return render_template('uploading.html')
+
+
 
 if __name__==" __main__ ":
     app.run(host ='0.0.0.0',debug=True)
